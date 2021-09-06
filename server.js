a5a9629cac8d01c13190cb066967dbb9b71ec4b2
kartikeyatitu <kartikeya2998@gmail.com>
2021-09-06 17:36:14 +0530

    change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("src/Database/db.json");
 const middlewares = jsonServer.defaults({ static: "./build" });
-const port =  8001;
+const port =  process.env.PORT || 8000;
 
 server.use(middlewares);
 server.use(router);
